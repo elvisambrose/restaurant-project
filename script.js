e18be2c1308c30a3ccc101c45fe059e2f80fe656
+ <+>
2026-02-11 11:46:38 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
       form.addEventListener("submit", function(e){
         e.preventDefault();
         let name = document.getElementById("name").value;
+        let date = document.getElementById("date").value;
+        let time = document.getElementById("time").value;
+        
         localStorage.setItem("reservationName", name);
-        alert("Reservation saved for " + name);
+        localStorage.setItem("reservationDate", date);
+        localStorage.setItem("reservationTime", time);
+        
+        alert("Reservation saved for " + name + " on " + date + " at " + time);
+        
       });
     }
   
-    let savedName = localStorage.getItem("reservationName");
-    if(savedName){
-      document.getElementById("savedReservation").innerText = "Reservation for: " + savedName;
+let savedName = localStorage.getItem("reservationName");
+let savedDate = localStorage.getItem("reservationDate");
+let savedTime = localStorage.getItem("reservationTime");
+
+if(savedName && savedDate && savedTime){
+    let reservationInfo = "Reservation for: " + savedName + " on " + savedDate + " at " + savedTime;
+    let savedReservation = document.getElementById("savedReservation");
+    if(savedReservation){ 
+        savedReservation.innerText = reservationInfo;
     }
-  });
+}  
+});
   
